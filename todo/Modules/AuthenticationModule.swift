da49ef1704d2a4447ffa5de8ac73dac80c79851e
Gheorghe Curca <curcagheorghe@gmail.com>
2023-10-27 10:40:05 +0300

    Added integration with firebase

---
+++ todo/Modules/AuthenticationModule.swift
@@ -0,0 +1,12 @@
+//
+//  AuthenticationModule.swift
+//  todo
+//
+//  Created by Gheorghe Curca on 27.10.2023.
+//
+
+import Foundation
+
+class AuthenticationModule: ObservableObject {
+    static let shared = AuthenticationModule()
+}
---
+++ todo/ViewsModel/MainViewViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MainViewViewModel.swift
+//  todo
+//
+//  Created by Gheorghe Curca on 27.10.2023.
+//
+
+import Foundation
+
+class MainViewViewModel: ObservableObject {
+    
+}

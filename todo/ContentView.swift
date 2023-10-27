aaeba5785080ae0c38b2878026dbae0baa7bc8b5
Gheorghe Curca <curcagheorghe@gmail.com>
2023-10-27 10:25:43 +0300

    Init project

---
+++ todo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  todo
+//
+//  Created by Gheorghe Curca on 27.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Gheorghe Curca on 27.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
